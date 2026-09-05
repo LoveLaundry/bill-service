738bca8b475ef2f4851f5e1b24319dd35331409e
DulshanSiriwardhana <dulshansiriwardhanaofficial@gmail.com>
2026-09-06 00:32:28 +0530

    fix: restore missing ShopBill model imports

--- src/bill_service/routers/shop_bills.py
+++ src/bill_service/routers/shop_bills.py
@@ -20,6 +20,11 @@ from ..database.main_db import (
 from ..models import (
     BillTemplateCreate,
     ShopBillBulkStatus,
+    ShopBillCreate,
+    ShopBillPayment,
+    ShopBillSplit,
+    ShopBillMerge,
+    ShopBillUpdate,
 )
 from ..repositories.main_repository import bump_version, enqueue_sync
 from ..router_utils import log_audit, parse_object_id
